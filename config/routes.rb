96342c4b6ad5a59210f77505f87407815cb8276d
akn127 <sbf-neppie@outlook.jp>
2021-03-06 17:24:45 +0900

    サインイン、サインアップ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  root to: "records#index"
 end
