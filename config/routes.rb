cbb2cf10cbcf0ae04a88e3b9b2481f677694ec55
akn127 <sbf-neppie@outlook.jp>
2021-03-08 12:31:53 +0900

    recordsテーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   root to: "records#index"
+  resources :records, only: :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_053713) do
+ActiveRecord::Schema.define(version: 2021_03_08_023849) do
+
+  create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.integer "category_id", null: false
+    t.date "date", null: false
+    t.string "place", null: false
+    t.string "with"
+    t.text "text"
+    t.string "url"
+    t.integer "status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_records_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_03_05_053713) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "records", "users"
 end
