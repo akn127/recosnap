0b73508aca350d0fa623aacc35f583de84a1b55c
akn127 <sbf-neppie@outlook.jp>
2021-03-09 18:56:39 +0900

    投稿編集・削除機能実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -16,6 +16,26 @@ class RecordsController < ApplicationController
     end
   end
 
+  def show
+    @record = Record.find(params[:id])
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    @record.update(record_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    redirect_to root_path
+  end
+
   private
 
   def record_params
