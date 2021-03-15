65b5a30279cb161484c7f4ca5c11670320debdf6
akn127 <sbf-neppie@outlook.jp>
2021-03-15 16:15:32 +0900

    favoriteモデル、ルーティング作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :records, dependent: :destroy
-
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
+    resources :favorites, onry: [:create, :destroy]
   end
   resources :lists, only: [:index]
+  resources :users
 end
